ac01743d310be7cf843c5acfdf50aa15e13aaf79
Neetesh <neetesht@gmail.com>
2022-07-12 13:53:20 -0400

    Add components

--- src/app/modules/home/home-card/home-card.component.ts
+++ src/app/modules/home/home-card/home-card.component.ts
@@ -7,6 +7,14 @@ import { lorem30 } from '../utils/text';
   styleUrls: ['./home-card.component.css'],
 })
 export class HomeCardComponent implements OnInit {
+  // add tour of hero component
+  // add an exp component
+  // add an pokemon component
+  // add routing at the route
+  // make home cards clickable and route
+
+  // add routing
+
   @Input() title: string = 'Feature';
   @Input() icon: string = 'science';
   @Input()
